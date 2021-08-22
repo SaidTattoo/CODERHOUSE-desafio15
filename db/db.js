3a084c4cfe50bd4122520920c4ed2e57088a5f01
SaidTattoo <said.tattoo@gmail.com>
2021-08-22 01:01:26 -0400

    correccion de crear tabla si no existe

--- db/db.js
+++ db/db.js
@@ -1,6 +1,8 @@
-const knef = require('knex')
+const knex = require('knex');
 const knexFile = require('./knexfile')
 const { NODE_ENV } =  require('../config/globals')
 
-const db = knef(knexFile[NODE_ENV])
+
+
+const db = knex(knexFile[NODE_ENV])
 module.exports = db
